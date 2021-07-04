e0b0c84994dd1abf47cc792d8fd907e0711e394a
Jonathan Ramses <abjonathan@hotmail.com>
2021-07-04 17:24:57 -0300

    splash-screen e signin feitos

--- App.tsx
+++ App.tsx
@@ -1,19 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View , Text } from 'react-native';
+import { Signin } from './src/screens/Signin'
 
-export default function App() {
+export default function App(){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+  <Signin />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
